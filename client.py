9baed140e8e420f1112cee872ac1f2ba02400044
Lino Bigatti <Lino Bigatti>
2020-06-27 02:49:05 -0300

    Bruh

---
+++ client.py
@@ -0,0 +1,37 @@
+#Mockup input script
+
+import hashlib
+import requests
+import json
+import urllib3
+urllib3.disable_warnings()  #Disable urllib warnings
+
+config = {}     #Load configuration.
+with open('config.json', 'r') as f:
+    config = json.load(f)
+#Configure ip
+ip = 'http://' + config["ip"] if config["ip"] != 'localhost' else 'http://127.0.0.1'
+ip += ':' + str(config["port"]) + '/'
+
+print("ip: " + ip)
+
+sha256 = hashlib.sha256()   #Start a hasher
+name = input('name: ')      #Get the name
+sha256.update(bytes(input("password: "), encoding='utf8'))
+pwd = sha256.hexdigest()    #Hash the password
+
+#Payload
+data1 = b'{"test": "e"}'
+#data2 = bytes(', "name": "' + name + '", "password": "' + pwd + '"}', encoding='utf8')
+
+while True:     #Command handler
+    r = None
+
+    op = input("Operation: (post/exit) ")
+    if op == "post":  #Send a POST to sign in
+        r = requests.post(ip + 'post', data=data1)
+    elif op == "exit":  #Exit
+        exit()
+
+    print(r)
+    print(r.text)
--- index.js
+++ index.js
@@ -2,18 +2,23 @@ const express = require('express')
 const compression = require('compression')
 const fs = require('fs');
 const app = express();
-app.use(compression())
+app.use(compression());
 
-app.get('/post', (req, res) => {
-	res.send('Post')
+function parsePost(req) {
+	console.log(req.body.test);
+}
+
+app.post('/post', (req, res) => {
+	var response = parsePost(req);
+	res.send(response)
 });
 
 app.get('/get', (req, res) => {
-	res.send('Getpost')
+	res.send('Getpost');
 });
 
 var config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
 
 app.listen(config["port"], () => {
-	console.log(`Backend running on ${config["port"]}`)
+	console.log(`Backend running on ${config["port"]}`);
 });
