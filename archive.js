39822b3af72ad207ae1b1e74b0452a935315cdd8
Lino Bigatti <Lino Bigatti>
2020-09-02 22:19:21 -0300

    Add a table showing ls results.

--- archive.js
+++ archive.js
@@ -2,7 +2,45 @@
 Litematic archive handling
 */
 
+const fs = require('fs');
+
 const index = (req, res) => {
-	res.send('test');
+	var lsFiles = fs.readdirSync('./archive/');
+	
+	var response = 	`
+					<table>
+					<thead>
+  						<tr>
+    						<th>Name</th>
+    						<th>Size</th>
+    						<th>Created</th>
+    						<th>Download</th>
+  						</tr>
+					</thead>
+					<tbody>
+					`
+
+	lsFiles.forEach(file => {
+		var stats= fs.statSync(`./archive/${file}`);
+
+		name = file;
+		size = stats.size;
+		created = stats.ctime;
+
+		response +=	`
+					<tr>
+    					<td>${name}</td>
+    					<td>${size}</td>
+    					<td>${created}</td>
+    					<td>Click here</td>
+  					</tr>
+  					`
+	});
+	response +=	`
+				</tbody>
+				</table>
+				`
+
+	res.send(response);
 }
 exports.index = index;
