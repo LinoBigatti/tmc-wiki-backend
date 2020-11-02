89e188b26cef1dbbf6e728198ac9a3d80f14221b
Joe <burtonjae@hotmail.co.uk>
2020-11-02 11:41:24 +0000

    Some security fixes

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const fileUpload = require('express-fileupload');
 
 const posts = require('./posts');
 const archive = require('./archive');
+const utils = require('./utils');
 
 const app = express();
 app.use(bodyParser.json());
@@ -18,7 +19,7 @@ app.use(xss());
 app.use(compression());
 app.use(fileUpload({createParentPath: true}));
 //assumes that production and development are false and that you need to supply at least one argument
-var production, development = false;
+let production = false, development = false;
 if (process.argv.length === 2) {
 	console.error('Expected at least one argument!');
 	process.exit(1);
@@ -32,15 +33,21 @@ else {
 	development = true;
 }
 console.log('arguments: ', arguments);
+
 const createPost = async (req, res) => {
-	const post = new posts.Post(req.body.body, req.body.title, req.body.tags, req.body.description, 0);
+	const reqBody = utils.cast('object', req.body);
+	const body = utils.cast('string', reqBody.body);
+	const title = utils.cast('string', reqBody.title);
+	const tags = utils.cast('string', reqBody.tags);
+	const description = utils.cast('string', reqBody.description);
+	const post = new posts.Post(body, title, tags, description, 0);
 	console.log(`Created post #${post.id}`);
 	await post.save();
 	res.send('OK')
 }
 
 const getPost = (req, res) => {
-	const postId = req.query.id;
+	const postId = utils.cast('number', utils.cast('object', req.query).id);
 	const post = posts.getPostMetadata()[postId - 1];
 	post.editCount = post.editCount || post.edit_count || 0;
 	post.body = posts.getBody(post.id)
@@ -49,7 +56,8 @@ const getPost = (req, res) => {
 	res.send(post);
 }
 const getPost_ = (req, res) => {
-	const postId = req.body.id;
+	const reqBody = utils.cast('object', req.body);
+	const postId = utils.cast('number', reqBody.id);
 	const post = posts.getPostMetadata()[postId - 1];
 	post.editCount = post.editCount || post.edit_count || 0;
 	post.body = posts.getBody(post.id)
@@ -57,14 +65,19 @@ const getPost_ = (req, res) => {
 }
 
 const editPost = async (req, res) => {
-	const postId = req.query.id;
+	const postId = utils.cast('number', utils.cast('object', req.query).id);
 	const oldPost = posts.getPostMetadata()[postId - 1];
 	oldPost.editCount = oldPost.editCount || oldPost.edit_count || 0;
-	if (oldPost.editCount !== req.body.lastEditCount) {
+	if (oldPost.editCount !== utils.cast('number', req.body.lastEditCount)) {
 		res.send('OUTDATED');
 		return;
 	}
-	const post = new posts.Post(req.body.body, req.body.title, req.body.tags, req.body.description, oldPost.editCount + 1);
+	const reqBody = utils.cast('object', req.body);
+	const body = utils.cast('string', reqBody.body);
+	const title = utils.cast('string', reqBody.title);
+	const tags = utils.cast('string', reqBody.tags);
+	const description = utils.cast('string', reqBody.description);
+	const post = new posts.Post(body, title, tags, description, oldPost.editCount + 1);
 	post.setId(postId);
 	post.edit_time(new Date().toDateString());
 	console.log(`Edited post #${post.id}`);
@@ -93,7 +106,7 @@ if(production) {
 	app.get('/__allposts__', getAllPosts);
 	app.get('/__latestposts__', latestPosts);
 
-	app.get('/archive/*', archive.download);
+	app.get('/archive/:fileName', archive.download);
 	app.get('/archive', archive.index);
 	app.post('/__archive-upload__', archive.uploadProcess);
 }
@@ -119,7 +132,7 @@ app.get('/archive/*', archive.download);
 app.get('/archive', archive.index);
 app.post('/__archive-upload__', archive.uploadProcess);
 
-var config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
+const config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
 
 app.listen(config["port"], () => {
 	console.log(`Backend running on ${config["port"]}`);
